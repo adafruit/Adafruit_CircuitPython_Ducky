5189075fa866537b2d8d62ee85ada4b8ec12c473
dherrada <dylan.herrada@adafruit.com>
2021-07-02 10:43:49 -0400

    Changed import name in example

--- examples/ducky_simpletest.py
+++ examples/ducky_simpletest.py
@@ -6,13 +6,13 @@ import time
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-import ducky
+import adafruit_ducky
 
 time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
 keyboard = Keyboard(usb_hid.devices)
 keyboard_layout = KeyboardLayoutUS(keyboard)  # We're in the US :)
 
-duck = ducky.Ducky("duckyscript.txt", keyboard, keyboard_layout)
+duck = adafruit_ducky.Ducky("duckyscript.txt", keyboard, keyboard_layout)
 
 result = True
 while result is not False:
